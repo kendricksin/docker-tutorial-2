6ae8901dd485ad715e3acaaaecb26498085c5ed4
unknown <kendrick_sin@hotmail.com>
2024-06-09 15:56:29 +0800

    add stuff to init sql

--- init/01-init.sql
+++ init/01-init.sql
@@ -1,4 +1,14 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+
+USE mydatabase;
+
 CREATE TABLE IF NOT EXISTS items (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
+
+INSERT INTO items (name) VALUES ('Apples');
+INSERT INTO items (name) VALUES ('Bananas');
+INSERT INTO items (name) VALUES ('Carrots');
+INSERT INTO items (name) VALUES ('Bread');
+INSERT INTO items (name) VALUES ('Milk');
